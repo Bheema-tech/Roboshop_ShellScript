8c0b28ddedfbbf96dedca987981ba4c91a736a82
bheema-tech <bheemesha590@gmail.com>
2025-05-26 02:31:19 +0530

    Shell scripts fundamentals

--- 02-shipping.sh
+++ 02-shipping.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
@@ -10,20 +11,22 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
-
 mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root priveleges or not
 if [ $USERID -ne 0 ]
 then
-    echo -e "ERROR: $R ... try with Root access$N" | tee -a $LOG_FILE
-    exit 1
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
 else
-    echo -e "$G Script is running with Root access$N"
+    echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
 echo "Please enter root password to setup"
 read -s MYSQL_ROOT_PASSWORD
 
-# Validate Function declration
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
@@ -33,45 +36,56 @@ VALIDATE(){
         exit 1
     fi
 }
+
 dnf install maven -y &>>$LOG_FILE
-VALIDATE $? "Installation of maven"
-mkdir -p /app
-VALIDATE $? "Directory creation"
-id roboshop
+VALIDATE $? "Installing Maven and Java"
+
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "Creating roboshop system user"
 else
-   echo "user already created.. nothing to do"
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
 fi
-cd /app
-rm -rf /app/*   
-#when we run multiple times, we should not get error, before copying the data to app directopry, better to delete the existing one
+
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
+
 curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
-VALIDATE $? "downloading the coder"
-unzip  /tmp/shipping.zip &>>$LOG_FILE
-VALIDATE $? "unzipping the data to app directory"
-mvn clean package &>>$LOG_FILE
-VALIDATE $? "installing dependencies"
-mv target/shipping-1.0.jar shipping.jar 
-cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service &>>$LOG_FILE
-VALIDATE $? "copying shipping service filer"
-systemctl daemon-reload
+VALIDATE $? "Downloading shipping"
+
+rm -rf /app/*
+cd /app 
+unzip /tmp/shipping.zip &>>$LOG_FILE
+VALIDATE $? "unzipping shipping"
+
+mvn clean package  &>>$LOG_FILE
+VALIDATE $? "Packaging the shipping application"
+
+mv target/shipping-1.0.jar shipping.jar  &>>$LOG_FILE
+VALIDATE $? "Moving and renaming Jar file"
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Daemon Realod"
+
+systemctl enable shipping  &>>$LOG_FILE
+VALIDATE $? "Enabling Shipping"
+
 systemctl start shipping &>>$LOG_FILE
-VALIDATE $? "start the shipping service"
-systemctl enable shipping &>>$LOG_FILE
-VALIDATE $? "enable the shipping service"
+VALIDATE $? "Starting Shipping"
 
 dnf install mysql -y  &>>$LOG_FILE
 VALIDATE $? "Install MySQL"
 
-mysql -h mysql.bheemadevops.fun -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
+mysql -h mysql.daws84s.site -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    mysql -h mysql.bheemadevops.fun -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
-    mysql -h mysql.bheemadevops.fun -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  &>>$LOG_FILE
-    mysql -h mysql.bheemadevops.fun -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
+    mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  &>>$LOG_FILE
+    mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
     VALIDATE $? "Loading data into MySQL"
 else
     echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
@@ -79,11 +93,6 @@ fi
 
 systemctl restart shipping &>>$LOG_FILE
 VALIDATE $? "Restart shipping"
-
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
