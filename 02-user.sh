4d2df2fee5c809c0961acc5b13cc7e6f7c628f24
bheema-tech <bheemesha590@gmail.com>
2025-05-26 01:24:06 +0530

    Shell scripts fundamentals

---
+++ 02-user.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+
+mkdir -p $LOGS_FOLDER
+if [ $USERID -ne 0 ]
+then
+    echo -e "ERROR: $R ... try with Root access$N" | tee -a $LOG_FILE
+    exit 1
+else
+    echo -e "$G Script is running with Root access$N"
+fi
+
+# Validate Function declration
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disablement of nodejs"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "enable of nodejs20"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installation of nodejs"
+mkdir -p /app
+VALIDATE $? "Directory creation"
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating roboshop system user"
+else
+   echo "user already created.. nothing to do"
+fi
+cd /app
+rm -rf /app/*   
+#when we run multiple times, we should not get error, before copying the data to app directopry, better to delete the existing one
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
+VALIDATE $? "downloading the coder"
+unzip  /tmp/user.zip &>>$LOG_FILE
+VALIDATE $? "unzipping the data to app directory"
+npm install &>>$LOG_FILE
+VALIDATE $? "installing dependencies"
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service &>>$LOG_FILE
+VALIDATE $? "copying user service filer"
+systemctl daemon-reload
+systemctl start user &>>$LOG_FILE
+VALIDATE $? "start the user service"
+systemctl enable user &>>$LOG_FILE
+VALIDATE $? "enable the user service"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
